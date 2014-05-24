cf062392bc1713eb7125d645606a16cebcb07bb1
Jairo Diaz <jairo.diaz@codescrum.com>
2014-05-24 16:20:03 +0100

    Retrieving the leaderboard

---
+++ display_leaderboard.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var options = {
+  host: 'hacker-olympics-london.herokuapp.com',
+  port: 80,
+  path: '/leaderboard.json',
+  method: 'GET'
+};
+
+http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}).end();
